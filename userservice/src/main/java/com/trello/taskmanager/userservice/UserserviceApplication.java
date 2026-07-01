888fd2285c88cf8ff771cd6e602c3f1978b8155f
GhatgeRitesh <riteshghatge12345@gmail.com>
2026-07-01 16:49:37 +0530

    config setup gateway and central Config

--- userservice/src/main/java/com/trello/taskmanager/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/trello/taskmanager/userservice/UserserviceApplication.java
@@ -1,4 +1,4 @@
-package com.trello.taskmanager.apigateway;
+package com.trello.taskmanager.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ApigatewayApplication {
+public class UserserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApigatewayApplication.class, args);
+		SpringApplication.run(UserserviceApplication.class, args);
 	}
 
 }
---
+++ userservice/src/main/java/com/trello/taskmanager/userservice/restcontroller/healthcheck.java
@@ -0,0 +1,30 @@
+package com.trello.taskmanager.userservice.restcontroller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/healthcheck")
+public class healthcheck {
+
+     @GetMapping("/")
+    public String sendRespond(){
+          return "OK";
+     }
+
+     @GetMapping("/getList")
+    public ResponseEntity<?> getList(){
+         System.out.println("Printing List");
+         ArrayList<String> list= new ArrayList<>();
+         list.add("a");
+         list.add("b");
+         return new ResponseEntity<>(list, HttpStatus.OK);
+     }
+}
