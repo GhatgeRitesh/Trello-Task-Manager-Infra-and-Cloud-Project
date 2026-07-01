1f5577b719d6fbffd59397d46017c13664060df6
GhatgeRitesh <riteshghatge12345@gmail.com>
2026-07-01 11:22:24 +0530

    base infra setup

---
+++ centralconfig/src/main/java/com/trello/taskmanager/centralconfig/CentralconfigApplication.java
@@ -0,0 +1,13 @@
+package com.trello.taskmanager.centralconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralconfigApplication.class, args);
+	}
+
+}
